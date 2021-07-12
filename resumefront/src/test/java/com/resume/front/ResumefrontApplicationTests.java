86716125ff867cc30ddb023461171a3d398261d9
No1stone <jangws1003@naver.com>
2021-07-12 16:23:50 +0900

    first commit!

---
+++ resumefront/src/test/java/com/resume/front/ResumefrontApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resume.front;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumefrontApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
