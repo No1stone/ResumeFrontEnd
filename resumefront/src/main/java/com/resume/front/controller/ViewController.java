bd25c0d4d75d0abe125a6be4fe1fd264d30c0739
No1stone <jangws1003@naver.com>
2021-07-12 16:38:35 +0900

    controller create

--- resumefront/src/main/java/com/resume/front/controller/ViewController.java
+++ resumefront/src/main/java/com/resume/front/controller/ViewController.java
@@ -1,10 +1,23 @@
 package com.resume.front.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ViewController {
 
-	
+	private static final Logger log = LoggerFactory.getLogger(ViewController.class);
+
+	@RequestMapping(value="/login")
+	public String login(@RequestParam("code") String code) {
+	  
+		log.info("kakaoCode: ----{}",code);
+		
+	    return "dashboard";
 	
 }
+}
+
